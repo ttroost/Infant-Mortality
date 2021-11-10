dab557b11826f032dbc092cd26d14649e2fc631b
ttroost <91199009+ttroost@users.noreply.github.com>
2021-11-10 12:11:03 +0100

    Update code.py

--- code.py
+++ code.py
@@ -53,10 +53,7 @@ st.write(fig)
 gapminder_2019 = gapminder[gapminder['year']==2007]
 
 
-fig = sns.boxplot(y='lifeExp', x='continent', 
-                 data=gapminder_2019, 
-                 width=0.5,
-                 palette="colorblind").set_title('Life expectancy per continent in 2019')
+fig = sns.boxplot(y='lifeExp', x='continent', data=gapminder_2019, width=0.5, palette="colorblind").set_title('Life expectancy per continent in 2019')
 st.write(fig)
 
 
