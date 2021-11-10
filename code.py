08264ec15d644f9211ae9266ac505a2755eab8ec
ttroost <91199009+ttroost@users.noreply.github.com>
2021-11-10 15:01:58 +0100

    Update code.py

--- code.py
+++ code.py
@@ -103,7 +103,7 @@ if sidebar_keuze == 'Infant mortality analysis':
     fig = px.line(mortality_africa, x = 'Year', y = 'mortality rate', title = 'Child mortality in Africa over the years')
     st.write(fig)
   
-  fig = go.Figure(df2)
+  fig = px.imshow(df2)
   st.write(fig)
 
 elif sidebar_keuze == 'Sources':
