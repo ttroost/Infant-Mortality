ed024f974ab7ce0b500e2559c937f04153654b5c
ttroost <91199009+ttroost@users.noreply.github.com>
2021-11-10 15:27:31 +0100

    Update code.py

--- code.py
+++ code.py
@@ -12,6 +12,15 @@ import ppscore as pps
 import seaborn as sns
 import plotly.graph_objects as go
 
+CURRENT_THEME ='green'
+IS_DARK_THEME = False
+
+[theme]
+primaryColor = "#575fe8"
+backgroundColor = "#f5f7f3"
+secondaryBackgroundColor = "#c9eab8"
+textColor = "#262730"
+font = "sans serif"
 
 # import dataset 
 data = pd.read_csv('Life_Expectancy_Data (1).csv')
