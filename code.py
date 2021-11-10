12c002c1d9ee4563162ebf9248855c7dffc312d9
ttroost <91199009+ttroost@users.noreply.github.com>
2021-11-10 13:58:56 +0100

    Update code.py

--- code.py
+++ code.py
@@ -105,8 +105,8 @@ if sidebar_keuze == 'Infant mortality analysis':
       st.write(fig)
 
   matrix_df = pps.matrix(df2)[['x', 'y', 'ppscore']].pivot(columns='x', index='y', values='ppscore')
-  fig = sns.heatmap(matrix_df, vmin=0, vmax=1, cmap="Blues", linewidths=0.5, annot=True)
-  st.write(fig)
+  fig2 = sns.heatmap(matrix_df, vmin=0, vmax=1, cmap="Blues", linewidths=0.5, annot=True)
+  st.write(fig2)
       
 
 elif sidebar_keuze == 'Sources':
